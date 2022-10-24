8c514989345a6afe7db50899e076ed0edc2df1c5
Romenildo Ferreira <romenildoferreira@gmail.com>
2022-10-24 18:10:27 -0300

    feat: controller motorista

---
+++ ProjetoMinsait/Controllers/MotoristaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMinsait.Models;
+using ProjetoMinsait.Repository.Interfaces;
+
+namespace ProjetoMinsait.Controllers
+{
+    [Route("v1/api/[controller]")]
+    [ApiController]
+    public class MotoristaController : ControllerBase
+    {
+
+        private readonly IMotoristaRepositorio _motoristaRepositorio;
+
+        public MotoristaController(IMotoristaRepositorio motoristaRepositorio)
+        {
+            _motoristaRepositorio = motoristaRepositorio;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Motorista>>> BuscarTodosMotoristas()
+        {
+            List<Motorista> resultado = await _motoristaRepositorio.BuscarTodosMotoristas();
+            return Ok(resultado);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Motorista>> BuscarPorID(int id)
+        {
+            Motorista resultado = await _motoristaRepositorio.BuscarPorID(id);
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Motorista>> Cadastrar([FromBody] Motorista motorista) 
+        {
+            Motorista resultado = await _motoristaRepositorio.Adicionar(motorista);
+            return Ok(resultado);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Motorista>> Atualizar(int id, [FromBody] Motorista motorista)
+        {
+            motorista.Id = id;
+            Motorista resultado = await _motoristaRepositorio.Atualizar(id, motorista);
+            return Ok(resultado);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Deletar(int id)
+        {
+            bool resultado = await _motoristaRepositorio.Deletar(id);
+            return Ok(resultado);
+        }
+
+    }
+}
