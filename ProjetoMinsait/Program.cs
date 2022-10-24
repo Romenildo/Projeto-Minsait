d9dd22bc80f090558fbc5af14691a685f563ffc6
Romenildo Ferreira <romenildoferreira@gmail.com>
2022-10-24 17:32:51 -0300

    build: configuracao com banco de dados sqlServer

--- ProjetoMinsait/Program.cs
+++ ProjetoMinsait/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoMinsait.Data;
+using ProjetoMinsait.Repository;
+using ProjetoMinsait.Repository.Interfaces;
+
 namespace ProjetoMinsait
 {
     public class Program
@@ -7,11 +12,23 @@ namespace ProjetoMinsait
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+           
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //connection with sql server
+            var connectionStringDB = builder.Configuration.GetConnectionString("DataBase");
+            builder.Services.AddEntityFrameworkSqlServer()
+                .AddDbContext<DataContext>(
+                    options => options.UseSqlServer(connectionStringDB)
+                );
+
+
+            //injecoes de dependencias
+            builder.Services.AddScoped<IMotoristaRepositorio, MotoristaRepositorio>();
 
             var app = builder.Build();
 
